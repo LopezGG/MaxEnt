d0abea99f729eaf5c014345d35940985509f169a
LopezGG <lopezgilsinia@gmail.com>
2016-02-07 21:23:43 -0800

    V1

---
+++ EmpiricalEstimation/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpiricalEstimation
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            string TrainingFile = args[0];
+            string OutPutFile = args[1];
+            string line;
+
+            //Dictionary<String,List<string>> TrainDocs = new Dictionary<string,List<string>>();
+            List<String> Vocab = new List<string>();
+            Dictionary<String, Dictionary<string, double>> ClassFeatureTemp = new Dictionary<string, Dictionary<string, double>>();
+            Dictionary<String, Dictionary<string, double>> ClassFeature = new Dictionary<string, Dictionary<string, double>>();
+            List<String> ClassList = new List<string>();
+            string classLabel,key;
+            int index,totalFeatures;
+            double value;
+            // this will give us basic counts for each feature in a class. we will have to convert it into prob
+            using(StreamReader Sr = new StreamReader(TrainingFile))
+            {
+                while((line = Sr.ReadLine())!=null)
+                {
+                    if (String.IsNullOrEmpty(line))
+                        continue;
+                    string[] words = line.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                    classLabel = words[0];
+                    ClassList.Add(classLabel);
+                    for (int i = 1; i < words.Length; i++)
+                    {
+                        index = words[i].IndexOf(":");
+                        key = words[i].Substring(0, index);
+                        value = Convert.ToDouble(words[i].Substring(index + 1));
+                        if (value <= 0)
+                            continue;
+                        Vocab.Add(key);
+                        if (ClassFeatureTemp.ContainsKey(classLabel) && ClassFeatureTemp[classLabel].ContainsKey(key))
+                            ClassFeatureTemp[classLabel][key]++;
+                        else if (ClassFeatureTemp.ContainsKey(classLabel))
+                            ClassFeatureTemp[classLabel].Add(key, value);
+                        else
+                        {
+                            Dictionary<string, double> temp = new Dictionary<string, double>();
+                            temp.Add(key, 1);
+                            ClassFeatureTemp.Add(classLabel, temp);
+                        }
+                    }
+
+                }
+            }
+            ClassList = ClassList.Distinct().ToList();
+            Vocab = Vocab.Distinct().ToList();
+            totalFeatures = ClassList.Count * Vocab.Count;
+            foreach (var cl in ClassList)
+            {
+                ClassFeature.Add(cl, new Dictionary<string, double>());
+                var features = ClassFeatureTemp[cl];
+                foreach (var word in Vocab)
+                {
+                    if (features.ContainsKey(word))
+                        ClassFeature[cl].Add(word, features[word] / totalFeatures);
+                    else
+                        ClassFeature[cl].Add(word, 0);//TODO: See if we have to add smoothing else this is not necessary
+
+                }
+                ClassFeature[cl]=ClassFeature[cl].OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            }
+
+
+            using (StreamWriter Sw = new StreamWriter(OutPutFile))
+            {
+                //write SysOutput
+                foreach (var clData in ClassFeature)
+                {
+                    var featList = clData.Value;
+                    Sw.WriteLine("FEATURES FOR CLASS " + clData.Key);
+                    foreach (var fl in featList)
+                    {
+                        Sw.WriteLine(fl.Key + " " + fl.Value);
+                    }
+
+                }
+
+            }
+            
+        }
+    }
+}
---
+++ MaxEnt/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxEnt
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            string TestFile = args[0];
+            string ModelFile = args[1];
+            string sysOutput = args[2];
+            string line;
+            Dictionary<String, int> ConfusionDict = new Dictionary<string, int>();
+
+            //read the model into a DS
+            Dictionary<String, Dictionary<String,double>> ModelFeatureClassProb = new Dictionary<string, Dictionary<String,double>>();
+            List<String> ClassList = new List<string>();
+            string classLabel="", key;
+            double value;
+            using(StreamReader Sr =  new StreamReader(ModelFile))
+            {
+                while((line = Sr.ReadLine())!=null)
+                {
+                    if (String.IsNullOrEmpty(line))
+                        continue;
+                    if (line.Contains("FEATURES FOR CLASS"))
+                    {
+                        classLabel = line.Substring(line.IndexOf("FEATURES FOR CLASS") + 19);
+                        ClassList.Add(classLabel);
+                        continue;
+                    }
+                    else
+                    {
+                        string[] words = line.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                        key = words[0];
+                        value = Convert.ToDouble(words[1]);
+                        if (ModelFeatureClassProb.ContainsKey(classLabel) && !ModelFeatureClassProb[classLabel].ContainsKey(key))
+                            ModelFeatureClassProb[classLabel].Add(key, value);
+                        else if (!ModelFeatureClassProb.ContainsKey(classLabel))
+                        {
+                            Dictionary<String,double> temp = new Dictionary<string,double>();
+                            temp.Add(key,value);
+                            ModelFeatureClassProb.Add(classLabel, temp);
+                        }
+                            
+
+                    }
+                }
+            }
+            ClassList = ClassList.Distinct().ToList();
+
+            string actualClass = "",predictedClass="";
+            int index,totalDoc=0;
+            StreamWriter Sw = new StreamWriter(sysOutput);
+            //reading the TestFile
+            using (StreamReader Sr = new StreamReader(TestFile))
+            {
+                while((line = Sr.ReadLine())!=null)
+                {
+                    if (String.IsNullOrEmpty(line))
+                        continue;
+                    Dictionary<string, double> TestClassScore = new Dictionary<string, double>();
+                    string[] words = line.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                    actualClass = words[0];
+
+                    double totalScore = 0;
+                    foreach (var predClass in ClassList)
+                    {
+                        double score =0;
+                        var featureList = ModelFeatureClassProb[predClass];
+                        for (int i = 1; i < words.Length; i++)
+                        {
+                            index = words[i].IndexOf(":");
+                            key = words[i].Substring(0,index);
+                            value = Convert.ToDouble(words[i].Substring(index + 1));
+                            if (value <= 0)
+                                continue;
+                            if (featureList.ContainsKey(key))
+                                score += featureList[key];
+                            else
+                                score += featureList["<default>"];
+                        }
+                        TestClassScore.Add(predClass, score);
+                        totalScore += score;
+                    }
+                    //sorting these and dividing by total score to get class prob from scores
+                    TestClassScore = TestClassScore.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value / totalScore);
+                    //write to sysOutput
+                    Sw.Write("array:" + totalDoc + "\t");
+                    totalDoc++;
+                    foreach (var item in TestClassScore)
+                    {
+                        Sw.Write(item.Key + "\t" + item.Value + "\t");
+                    }
+                    Sw.WriteLine();
+                    predictedClass = TestClassScore.Keys.First();
+                    key = actualClass + "_" + predictedClass;
+                    if (ConfusionDict.ContainsKey(key))
+                        ConfusionDict[key]++;
+                    else
+                        ConfusionDict.Add(key, 1);
+
+                }
+            }
+            Sw.Close();
+
+
+            //Write confusion Matrix to console. totalDoc is ok without -- because we counted from zero when we wrote the document.
+            WriteConfusionMatrix(ClassList, ConfusionDict, "test", totalDoc);
+            Console.ReadLine();
+        }
+
+        public static void WriteConfusionMatrix (List<String> ClassBreakDown, Dictionary<String, int> ConfusionDict, string testOrTrain, int totalInstances)
+        {
+            int correctPred = 0;
+            Console.WriteLine("Confusion matrix for the " + testOrTrain + " data:\n row is the truth, column is the system output");
+            Console.Write("\t\t\t");
+            foreach (var actClass in ClassBreakDown)
+            {
+                Console.Write(actClass + "\t");
+            }
+            Console.WriteLine();
+            foreach (var actClass in ClassBreakDown)
+            {
+
+                Console.Write(actClass + "\t");
+                foreach (var predClass in ClassBreakDown)
+                {
+
+                    if (ConfusionDict.ContainsKey(actClass + "_" + predClass))
+                    {
+                        Console.Write(ConfusionDict[actClass + "_" + predClass] + "\t");
+                        if (actClass == predClass)
+                            correctPred += ConfusionDict[actClass + "_" + predClass];
+                    }
+                    else
+                        Console.Write("0" + "\t");
+
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(testOrTrain + " accuracy=" + Convert.ToString(correctPred / ( double )totalInstances));
+            Console.WriteLine();
+
+
+        }
+    }
+}
